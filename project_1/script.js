0383134ae55c5a1a8d2fbbebc830a53e7ab86182
Artyom Pavlovich <43780397+ArtyomPavlovich@users.noreply.github.com>
2018-12-12 18:40:29 +0300

    Add header and footer for project create footer add menu

---
+++ project_1/script.js
@@ -0,0 +1,6 @@
+//<div class="grid-items"><img src="ing/img_1.4.png" alt=""></div>
+//    <div class="grid-items"><img src="ing/img_1.5.png" alt=""></div>
+//    <div class="grid-items"><img src="ing/img_1.6.png" alt=""></div>
+//    <div class="grid-items"><img src="ing/img_1.7.png" alt=""></div>
+//    <div class="grid-items"><img src="ing/img_1.8.png" alt=""></div>
+//    <div class="grid-items"><img src="ing/img_1.9.png" alt=""></div>
